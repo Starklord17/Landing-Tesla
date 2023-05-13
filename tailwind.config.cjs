a64ab99ff974e24d89954a83b47447117087ec0b
Starklord17 <maxi.sm117@gmail.com>
2023-05-13 18:56:01 -0300

    :sparkles: Tailwind media queries

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,38 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+		  'sm': '640px',
+		  'md': '768px',
+		  'lg': '1024px',
+		  'xl': '1280px',
+		  '2xl': '1536px',
+		  'print': {'raw': 'print'},
+		},
 		fontFamily: {
-			sans: ['Gotham SSm A, sans-serif']
+		  sans: ['Gotham SSm A, sans-serif']
 		},
 		extend: {},
+	  },
+	  plugins: [],
+	}
+
+/* // tailwind.config.js
+module.exports = {
+	theme: {
+	  screens: {
+		'sm': '640px',
+		'md': '768px',
+		'lg': '1024px',
+		'xl': '1280px',
+		'2xl': '1536px',
+		'print': {'raw': 'print'},
+	  },
+	  fontFamily: {
+		sans: ['Gotham SSm A, sans-serif']
+	  },
+	  extend: {},
 	},
 	plugins: [],
-}
+  }
+   */
